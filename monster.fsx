f73a13e349984e4de84faf139f94fed8cf9fcc9f
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2018-07-20 18:39:27 -0700

    Added ability bonuses

--- monster.fsx
+++ monster.fsx
@@ -20,23 +20,45 @@ let scoreToModifier score =
     |> min 10
     |> max -5
 
+type AbilityScoreBonus = {
+    Ability: Ability
+    Bonus: int
+    }
+
+type Abilities = {
+    Scores: AbilityScores
+    Bonuses: AbilityScoreBonus list
+    }
+
 let modifier abilities ability = 
-    match ability with
-    | STR -> abilities.STR
-    | DEX -> abilities.DEX
-    | CON -> abilities.CON
-    | INT -> abilities.INT 
-    | WIS -> abilities.WIS
-    | CHA -> abilities.CHA
+    let baseScore = 
+        let scores = abilities.Scores
+        match ability with
+        | STR -> scores.STR
+        | DEX -> scores.DEX
+        | CON -> scores.CON
+        | INT -> scores.INT 
+        | WIS -> scores.WIS
+        | CHA -> scores.CHA
+    let bonuses = 
+        abilities.Bonuses 
+        |> List.sumBy (fun bonus -> 
+            if bonus.Ability = ability 
+            then bonus.Bonus 
+            else 0)
+    baseScore + bonuses 
     |> scoreToModifier
 
 let goblin = {
-    STR = 8
-    DEX = 14
-    CON = 10
-    INT = 10
-    WIS = 8
-    CHA = 8
+    Scores = {
+        STR = 8
+        DEX = 14
+        CON = 10
+        INT = 10
+        WIS = 8
+        CHA = 8
+        }
+    Bonuses = [ ]
     }
 
 modifier goblin DEX
