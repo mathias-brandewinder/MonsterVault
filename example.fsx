f2a25d444a0a5304788ce1054d7532443b925948
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2018-07-31 16:16:27 -0700

    Add Hobgoblin example

--- example.fsx
+++ example.fsx
@@ -6,29 +6,80 @@ open Monster.Ability
 open Markdown
 
 let goblin = {
-    Scores = {
-        STR = 8
-        DEX = 14
-        CON = 10
-        INT = 10
-        WIS = 8
-        CHA = 8
+    Name = "Goblin"
+    HitDice = 2
+    Size = Small       
+    Abilities = {
+        Scores = {
+            STR = 8
+            DEX = 14
+            CON = 10
+            INT = 10
+            WIS = 8
+            CHA = 8
+            }
+        Bonuses = [ ]
         }
-    Bonuses = [ ]
     }
 
-score goblin STR
-modifier goblin STR
+score goblin.Abilities STR
+modifier goblin.Abilities STR
 
 let goblinBoss = {
     goblin with
-        Bonuses = [
-            { Ability = STR; Bonus = 2 }
-            { Ability = CHA; Bonus = 2 }
-            ]
+        Name = "Goblin Boss"
+        HitDice = 6            
+        Abilities = {
+            goblin.Abilities with
+                Bonuses = [
+                    { Ability = STR; Bonus = 2 }
+                    { Ability = CHA; Bonus = 2 }
+                    ]
+        }
     }
 
-score goblinBoss STR
-modifier goblinBoss STR
+score goblinBoss.Abilities STR
+modifier goblinBoss.Abilities STR
 
-let abilityBlock = goblinBoss |> Markdown.abilities
+let abilityBlock = 
+    goblinBoss.Abilities 
+    |> Markdown.abilities
+
+let hobgoblin = {
+    Name = "Hobgoblin"
+    HitDice = 2
+    Size = Medium       
+    Abilities = {
+        Scores = {
+            STR = 13
+            DEX = 12
+            CON = 12
+            INT = 10
+            WIS = 10
+            CHA = 9
+            }
+        Bonuses = [ ]
+        }
+    }
+
+let hobgoblinCaptain = {
+    hobgoblin with
+        Name = "Hobgoblin Captain"
+        HitDice = 6
+        Abilities = {
+            hobgoblin.Abilities with
+                Bonuses = [
+                    { Ability = STR; Bonus = 2 }
+                    { Ability = DEX; Bonus = 2 }
+                    { Ability = CON; Bonus = 2 }
+                    { Ability = INT; Bonus = 2 }
+                    { Ability = CHA; Bonus = 4 }
+                    ]
+        }
+    }
+
+hobgoblin |> Monster.HitPoints
+hobgoblin |> Monster.HitPoints |> Roll.Average
+
+hobgoblinCaptain |> Monster.HitPoints
+hobgoblinCaptain |> Monster.HitPoints |> Roll.Average
